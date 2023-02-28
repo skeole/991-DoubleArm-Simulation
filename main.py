cbd135364425b680ca5a5a83716c64e254669bcb
shaankeole <skeole24@brophybroncos.org>
2023-02-27 23:50:37 -0700

    added text

--- main.py
+++ main.py
@@ -243,6 +243,132 @@ def pointToAngles(coordinates, ccu=True):
     else:
         return [angle + first_angle, angle - second_angle]
 
+def anglesToPoint(angles):
+    return [
+        (proximal_length - 4) * math.cos(math.pi / 180.0 * angles[0]) + (distal_length + 7.751984) * math.cos(math.pi / 180.0 * angles[1]), 
+        (proximal_length - 4) * math.sin(math.pi / 180.0 * angles[0]) + (distal_length + 7.751984) * math.sin(math.pi / 180.0 * angles[1])
+    ]
+
+CharacterList = "abcdefghijklmnopqrstuvwxyz 0123456789(),.-"
+font = [[50, ['l', (50, 0), (50, 40)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], 
+            ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)]], #a
+         [50, ['l', (0, 100), (0, 0)], ['l', (0, 0), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], 
+            ['l', (50, 10), (50, 40)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)]], #b
+         [50, ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], 
+            ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)]], #c
+         [50, ['l', (50, 100), (50, 0)], ['l', (50, 10), (40, 0)], ['l', (40, 0), (10, 0)], ['l', (10, 0), (0, 10)], ['l', (0, 10), (0, 40)], ['l', (0, 40), (10, 50)], ['l', (10, 50), (40, 50)], ['l', (40, 50), (50, 40)]], #d
+         [50, ['l', (0, 30), (50, 30)], ['l', (50, 30), (50, 40)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)]], #e
+         [40, ['l', (40, 90), (30, 100)], ['l', (30, 100), (20, 100)], ['l', (20, 100), (10, 90)], ['l', (10, 90), (10, 0)], ['l', (0, 50), (30, 50)]], #f
+         [50, ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 40), (50, -40)], ['l', (50, -40), (40, -50)], ['l', (40, -50), (10, -50)], ['l', (10, -50), (0, -40)], ['l', (0, -40), (0, -30)]], #g
+         [50, ['l', (0, 100), (0, 0)], ['l', (0, 40), (10, 50)], ['l', (10, 50), (40, 50)], ['l', (40, 50), (50, 40)], ['l', (50, 40), (50, 0)]], #h
+         [0, ['l', (0, 0), (0, 50)], ['l', (0, 70), (0, 70)]], #i
+         [10, ['l', (10, 50), (10, -40)], ['l', (10, -40), (0, -50)], ['l', (10, 70), (10, 70)]], #j
+         [30, ['l', (0, 100), (0, 0)], ['l', (0, 30), (30, 50)], ['l', (0, 30), (30, 0)]], #k
+         [0, ['l', (0, 100), (0, 0)]], #l
+         [60, ['l', (0, 0), (0, 50)], ['l', (0, 40), (10, 50)], ['l', (10, 50), (20, 50)], ['l', (20, 50), (30, 40)], ['l', (30, 0), (30, 40)], 
+            ['l', (30, 40), (40, 50)], ['l', (40, 50), (50, 50)], ['l', (50, 50), (60, 40)], ['l', (60, 0), (60, 40)]], #m
+         [40, ['l', (0, 50), (0, 0)], ['l', (0, 40), (10, 50)], ['l', (10, 50), (30, 50)], ['l', (30, 50), (40, 40)], ['l', (40, 40), (40, 0)]], #n
+         [50, ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 10), (50, 40)], ['l', (50, 40), (40, 50)]], #o
+         [50, ['l', (50, 40), (50, 10)], ['l', (50, 10), (40, 0)], ['l', (40, 0), (10, 0)], ['l', (10, 0), (0, 10)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, -50)]], #p
+         [50, ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 40), (50, -40)], ['l', (50, -40), (60, -50)]], #q
+         [50, ['l', (0, 50), (0, 0)], ['l', (0, 40), (10, 50)], ['l', (10, 50), (40, 50)], ['l', (40, 50), (50, 40)]], #r
+         [50, ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (10, 30)], ['l', (10, 30), (40, 20)], ['l', (40, 20), (50, 10)], ['l', (50, 10), (40, 0)], ['l', (40, 0), (10, 0)], ['l', (10, 0), (0, 10)]], #s
+         [40, ['l', (10, 100), (10, 10)], ['l', (10, 10), (20, 0)], ['l', (20, 0), (30, 0)], ['l', (30, 0), (40, 10)], ['l', (0, 50), (30, 50)]], #t
+         [50, ['l', (0, 50), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 50), (50, 0)]], #u
+         [50, ['l', (0, 50), (25, 0)], ['l', (25, 0), (50, 50)]], #v
+         [60, ['l', (0, 50), (20, 0)], ['l', (20, 0), (30, 30)], ['l', (30, 30), (40, 0)], ['l', (40, 0), (60, 50)]], #w
+         [50, ['l', (0, 50), (50, 0)], ['l', (50, 50), (0, 0)]], #x
+         [50, ['l', (0, 50), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 50), (50, -40)], ['l', (50, -40), (40, -50)], ['l', (40, -50), (10, -50)], ['l', (10, -50), (0, -40)]], #y
+         [50, ['l', (0, 50), (50, 50)], ['l', (50, 50), (0, 0)], ['l', (0, 0), (50, 0)]], #z
+         [30], #space
+         [50, ['l', (50, 90), (40, 100)], ['l', (40, 100), (10, 100)], ['l', (10, 100), (0, 90)], ['l', (0, 90), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (40, 0), (10, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 10), (50, 90)], ['l', (50, 80), (0, 20)]], #0
+         [40, ['l', (0, 80), (20, 100)], ['l', (20, 100), (20, 0)], ['l', (0, 0), (40, 0)]], #1
+         [50, ['l', (0, 80), (0, 90)], ['l', (0, 90), (10, 100)], ['l', (10, 100), (40, 100)], ['l', (40, 100), (50, 90)], ['l', (50, 90), (50, 60)], ['l', (50, 60), (0, 10)], ['l', (0, 10), (0, 0)], ['l', (0, 0), (50, 0)], ['l', (50, 0), (50, 10)]], #2
+         [50, ['l', (0, 80), (0, 90)], ['l', (0, 90), (10, 100)], ['l', (10, 100), (40, 100)], ['l', (40, 100), (50, 90)], ['l', (50, 90), (50, 60)], ['l', (50, 60), (40, 50)], ['l', (40, 50), (20, 50)], ['l', (40, 50), (50, 40)], ['l', (50, 40), (50, 10)], ['l', (50, 10), (40, 0)], ['l', (40, 0), (10, 0)], ['l', (10, 0), (0, 10)], ['l', (0, 10), (0, 20)], ['l', (10, 50), (20, 50)]], #3
+         [50, ['l', (0, 100), (0, 50)], ['l', (0, 50), (50, 50)], ['l', (50, 100), (50, 0)]], #4
+         [50, ['l', (50, 100), (0, 100)], ['l', (0, 100), (0, 50)], ['l', (0, 50), (40, 50)], ['l', (40, 50), (50, 40)], ['l', (50, 40), (50, 10)], ['l', (50, 10), (40, 0)], ['l', (40, 0), (10, 0)], ['l', (10, 0), (0, 10)]], #5
+         [50, ['l', (50, 90), (40, 100)], ['l', (40, 100), (10, 100)], ['l', (10, 100), (0, 90)], ['l', (0, 90), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 10), (50, 40)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (10, 50)], ['l', (10, 50), (0, 40)]], #6
+         [50, ['l', (0, 100), (50, 100)], ['l', (50, 100), (10, 0)], ['l', (40, 50), (20, 50)]], #7
+         [50, ['l', (50, 90), (40, 100)], ['l', (40, 100), (10, 100)], ['l', (10, 100), (0, 90)], ['l', (0, 90), (0, 60)], ['l', (0, 60), (10, 50)], ['l', (10, 50), (0, 40)], ['l', (0, 40), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (40, 0)], ['l', (40, 0), (50, 10)], ['l', (50, 10), (50, 40)], ['l', (50, 40), (40, 50)], ['l', (40, 50), (50, 60)], ['l', (50, 60), (50, 90)], ['l', (40, 50), (10, 50)]], #8
+         [50, ['l', (50, 90), (40, 100)], ['l', (40, 100), (10, 100)], ['l', (10, 100), (0, 90)], ['l', (0, 90), (0, 60)], ['l', (0, 60), (10, 50)], ['l', (10, 50), (40, 50)], ['l', (40, 50), (50, 60)], ['l', (50, 90), (50, 0)]], #9  
+         [20, ['l', (20, 100), (10, 100)], ['l', (10, 100), (0, 90)], ['l', (0, 90), (0, 10)], ['l', (0, 10), (10, 0)], ['l', (10, 0), (20, 0)]], #(
+         [20, ['l', (0, 100), (10, 100)], ['l', (10, 100), (20, 90)], ['l', (20, 90), (20, 10)], ['l', (20, 10), (10, 0)], ['l', (10, 0), (0, 0)]], #)
+         [3, ['l', (3, 0), (3, -10)], ['l', (3, -10), (0, -20)]], #,
+         [2, ['l', (0, 0), (0, 2)], ['l', (0, 2), (2, 2)], ['l', (2, 2), (2, 0)], ['l', (2, 0), (0, 0)]], #.
+         [30, ['l', (0, 50), (30, 50)]], #-
+         ]
+
+def polygon_for_line(point_1, point_2, width, smoothness=4):
+    radius = float(width)/2
+    if point_1[0] == point_2[0]:
+        x_min = point_1[0]
+        y_min = min(point_1[1], point_2[1])
+        x_max = point_1[0]
+        y_max = max(point_1[1], point_2[1])
+        angle = math.pi/2
+    else:
+        if point_1[0] < point_2[0]:
+            x_min = point_1[0]
+            y_min = point_1[1]
+            x_max = point_2[0]
+            y_max = point_2[1]
+        else:
+            x_max = point_1[0]
+            y_max = point_1[1]
+            x_min = point_2[0]
+            y_min = point_2[1]
+        angle = math.atan((y_max-y_min)/(x_max-x_min))
+    
+    polygon = []
+    
+    angle_modifier = -math.pi/2
+    for i in range(smoothness+1):
+        polygon.append((x_max + radius * math.cos(angle + angle_modifier), y_max + radius * math.sin(angle + angle_modifier)))
+        angle_modifier += math.pi/smoothness
+    angle_modifier -= math.pi/smoothness
+    for i in range(smoothness+1):
+        polygon.append((x_min + radius * math.cos(angle + angle_modifier), y_min + radius * math.sin(angle + angle_modifier)))
+        angle_modifier += math.pi/smoothness
+    
+    return polygon
+
+def translate(point, reference, width, height, italics, angle):
+    return (reference[0] + (point[0] + italics * point[1]) * math.cos(angle) * width + point[1] * math.sin(angle) * height, 
+            reference[1] - (point[0] + italics * point[1]) * math.sin(angle) * width + point[1] * math.cos(angle) * height)
+
+def letter_hitbox(data, point, weight, width, height, italics=0.0, angle=0, smoothness=3):
+    convertedAngle = - angle * math.pi / 180.0
+    hitbox = []
+    for i in data:
+        if i != data[0]: #data[0] = width
+            if i[0] == "l":
+                hitbox.append(polygon_for_line(translate(i[1], point, width, -height, italics, convertedAngle), 
+                                               translate(i[2], point, width, -height, italics, convertedAngle), 
+                                               weight))
+    return hitbox
+
+class Text_Engine(object):
+    def __init__(self, surface):
+        self.surface = surface
+    
+    def type(self, text, font, point, width, height, color, weight, angle=0, space_between_letters=0, italics=0.0):
+        cangle = angle * math.pi/180.0
+        temp = -float(space_between_letters)
+        word_hitbox = []
+        for i in text:
+            temp += font[CharacterList.index(i)][0] * width
+            temp += space_between_letters
+        current_distance = -temp/2
+        for i in text:
+            for j in letter_hitbox(font[CharacterList.index(i)], (point[0] + current_distance*math.cos(cangle), point[1] - current_distance*math.sin(cangle)), weight, width, height, italics=italics, angle=-angle):
+                word_hitbox.append(j)
+            current_distance += font[CharacterList.index(i)][0] * width + space_between_letters
+        for polygon in word_hitbox:
+            pygame.draw.polygon(self.surface, color, polygon)
+
+TE = Text_Engine(gameDisplay)
+
+
 from networktables import NetworkTables
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -261,11 +387,22 @@ while run:
     gameDisplay.fill(background_color)
     
     pygame.draw.rect(gameDisplay, green, (0, int(display_size[1] * 0.85), display_size[0], display_size[1]))
+    current_angles = (smartDashboard.getNumber("Current Angle 1", -440), smartDashboard.getNumber("Current Angle 2", 80))
+    target_angles = (smartDashboard.getNumber("Target 1st Angle", -60), smartDashboard.getNumber("Target 2nd Angle", 60))
     drawPolygons(
         (display_size[0] * 0.1, display_size[1] * 0.85), 
-        (smartDashboard.getNumber("Current Angle 1", -80), smartDashboard.getNumber("Current Angle 2", 80)), 
-        (smartDashboard.getNumber("Target 1st Angle", -60), smartDashboard.getNumber("Target 2nd Angle", 60))
+        current_angles, 
+        target_angles
     )
+    
+    if (current_angles[0] > -360):
+        point = anglesToPoint(current_angles)
+        TE.type("target position is (" + str(round(point[0], 1)) + ", " + str(round(point[1], 1)) + ")", font, (int(display_size[0] * 0.5), 80), 0.5, 0.5, black, 4, space_between_letters=6)
+        
+        point = anglesToPoint(target_angles)
+        TE.type("current position is (" + str(round(point[0], 1)) + ", " + str(round(point[1], 1)) + ")", font, (int(display_size[0] * 0.5), 140), 0.5, 0.5, black, 4, space_between_letters=6)
+    else: # no reading from smartdashboard
+        TE.type("could not connect to smart dashboard", font, (int(display_size[0] * 0.5), 80), 0.4, 0.4, black, 3, space_between_letters=5)
 
     pygame.display.update()
     clock.tick(20)
